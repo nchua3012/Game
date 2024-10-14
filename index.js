47f8113ae1ecef7992cedca66721597a79c59d58
nchua3012 <n.chua3012@googlemail.com>
2024-10-14 17:05:11 +0100

    Updates and new game following tutorial

---
+++ index.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector ('canvas')
+const c = canvas.getContext ('2d')
+canvas.width = 1280
+canvas.height = 720
+
+c.fillStyle = 'beige'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image (/Assets)
+image.src = 'reference image'
