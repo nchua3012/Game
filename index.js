9c91f7af7624116981267bece64adfb2e3379777
nchua3012 <n.chua3012@googlemail.com>
2024-10-15 09:42:57 +0100

    Update to Game

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ canvas.height = 720
 c.fillStyle = 'beige'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const image = new Image (/Assets)
-image.src = 'reference image'
+const image = new Image ()
+image.src = 'Assets/Map/Map1.tmx'
